92706cdb1bd87395d5eb307479408d20e2edd378
anovsiradj <anov.siradj22@gmail.com>
2016-11-24 08:55:57 +0700

    facade as is

--- fn-facade.php
+++ fn-facade.php
@@ -1,12 +1,14 @@
 <?php
-function cutter($field) {
-	anovsiradj\Cutter::init()->field($field);
+use anovsiradj\Cutter;
+
+function cutter_field($field) {
+	Cutter::init()->field($field);
 }
 
 function cutter_start($name) {
-	anovsiradj\Cutter::init()->start($name);
+	Cutter::init()->start($name);
 }
 
 function cutter_end() {
-	anovsiradj\Cutter::init()->end();
+	Cutter::init()->end();
 }
