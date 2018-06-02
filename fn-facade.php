4f69999c7e75bb550fe703a1f86121fd8f4794c5
anovsiradj <anov.siradj22@gmail.com>
2018-06-03 02:51:11 +0900

    2.0.0 facade. check function if exists.

--- fn-facade.php
+++ fn-facade.php
@@ -1,14 +1,31 @@
 <?php
+
+/**
+* 
+* use anovsiradj/Cutter::init()->facade()
+* or anovsiradj/Cutter::facade()
+* to use this functions.
+* 
+* @since 20180602342132
+* 
+*/
+
 use anovsiradj\Cutter;
 
-function cutter_field($field) {
-	Cutter::init()->field($field);
+if (function_exists('cutter_field') === false) {
+	function cutter_field($field) {
+		Cutter::init()->field($field);
+	}
 }
 
-function cutter_start($name) {
-	Cutter::init()->start($name);
+if (function_exists('cutter_start') === false) {
+	function cutter_start($name) {
+		Cutter::init()->start($name);
+	}
 }
 
-function cutter_end() {
-	Cutter::init()->end();
+if (function_exists('cutter_end') === false) {
+	function cutter_end() {
+		Cutter::init()->end();
+	}
 }
