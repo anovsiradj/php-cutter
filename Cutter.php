64da792b39b544ebf334f248043f9f45ed38ca02
anovsiradj <anov.siradj22@gmail.com>
2018-07-24 22:49:23 +0900

    rewriting cutter script.
    change php-5.6 array style.
    so anyone with php-5.* can using this.
    
    change php-5.6 Variable-length argument lists.
    because..., reason.
    
    cutter->field() using foreach instead implode.
    because i doubt implode, it's so stupid.
    
    simple errors message. not too fancy.

--- Cutter.php
+++ Cutter.php
@@ -14,11 +14,13 @@ use Exception;
 
 class Cutter
 {
-	private static $instance;
-	protected $cfg = [];
+	const VERSION = '3.0.0';
 
-	protected $dataset = [];
-	protected $viewset = [];
+	protected static $instance;
+	protected $cfg = array();
+
+	protected $dataset = array();
+	protected $viewset = array();
 
 	private function __construct()
 	{
@@ -33,7 +35,7 @@ class Cutter
 
 	public static function &init()
 	{
-		if (isset(static::$instance) === false) static::$instance = new self;
+		if (isset(static::$instance) === false) static::$instance = new static;
 		return static::$instance;
 	}
 
@@ -48,73 +50,62 @@ class Cutter
 		}
 	}
 
-	public function __set($k, $v)
-	{
-		$this->set($k, $v);
-	}
-
 	public function get($mixed = null)
 	{
-		if ($mixed === null) {
+		if (empty($mixed)) {
 			return $this->cfg;
-		} elseif (is_string($mixed) && array_key_exists($mixed, $this->cfg)) {
+		} elseif (is_string($mixed) && isset($this->cfg[$mixed])) {
 			return $this->cfg[$mixed];
 		}
 		return null;
 	}
 
-	public function __get($k)
+	public function dataReference(&$data)
 	{
-		return $this->get($k);
+		foreach ($data as $k => &$v) {
+			$this->dataset[$k] = &$v;
+		}
 	}
 
-	public function data(...$args)
+	public function data($mixed, $v = null)
 	{
-		if (isset($args[0])) {
-			if (is_array($args[0])) {
-				foreach ($args[0] as $k => $v) {
-					$this->dataset[$k] = $v;
-				}
-			} elseif (is_string($args[0])) {
-				$v = isset($args[1]) ? $args[1] : null;
-				$this->dataset[$args[0]] = $v;
-			}
-			unset($args);
-
-		} else {
-			return $this->dataset;
+		if (is_array($mixed)) {
+			$this->dataReference($mixed);
+		} elseif (is_string($mixed)) {
+			$this->dataset[$mixed] = $v;
 		}
 	}
 
 	public function path($name)
 	{
-		$path = trim(rtrim($this->cfg['path'],'/\\'));
-		$path = empty($path) ? $path : ($path . '/');
-		return ($path . trim($name, '/\\') . '.cutter.php');
+		$path = empty($this->cfg['path']) ? $this->cfg['path'] : ($this->cfg['path'] . '/');
+		return ($path . $name . '.cutter.php');
 	}
 
-	public function view($name, $data = [], $render = true)
+	public function view($name, $data = array(), $render = true)
 	{
-		$this->data($data);
-		cutter_load_file($this->path($name), $this->dataset, true);
+		$this->dataReference($data);
+		_cutter_include($this->path($name), $this->dataset, true);
 
 		if ($render) $this->render();
 	}
 
-	public function render($data = [])
+	public function render($data = array())
 	{
-		if ($this->cfg['current_field'] !== null) throw new Exception(sprintf('<div>ErrorException <b>%s</b>: You have to close field to render template.</div>', __CLASS__));
+		if ($this->cfg['current_field'] !== null) {
+			throw new \Exception('[Cutter]: You have to close field to render template.');
+		}
 		if ($this->cfg['rendered']) return;
 		$this->cfg['rendered'] = true;
 
-		$this->data($data);
-		cutter_load_file($this->path($this->cfg['layout']), $this->dataset, false);
+		$this->dataReference($data);
+		_cutter_include($this->path($this->cfg['layout']), $this->dataset, false);
 	}
 
 	public function field($name)
 	{
 		if (array_key_exists($name, $this->viewset)) {
-			echo implode(null, $this->viewset[$name]);
+			foreach ($this->viewset[$name] as &$field) echo $field;
 			return true;
 		}
 		return false;
@@ -122,44 +113,55 @@ class Cutter
 
 	public function start($name, $stack = null)
 	{
-		if ($this->cfg['current_field'] !== null) throw new Exception(sprintf('<div>ErrorException <b>%s</b>: starting field <b>%s</b>, without ending <b>%s</b> field</div>', __CLASS__, $name, $this->cfg['current_field']));
+		if ($this->cfg['current_field'] !== null) {
+			throw new \Exception(sprintf('[Cutter]: opening field "%s", without closing "%s" field', $name, $this->cfg['current_field']), 1);
+		}
 
-		if (array_key_exists($name, $this->viewset) === false) $this->viewset[$name] = [];
+		if (!array_key_exists($name, $this->viewset)) $this->viewset[$name] = array();
 		$this->cfg['current_field'] = $name;
 		$this->cfg['current_stack'] = $stack;
 		ob_start();
 	}
+	public function begin($name, $stack = null)
+	{
+		$this->start($name, $stack);
+	}
 
 	public function end()
 	{
-		if ($this->cfg['current_field'] === null) throw new Exception(sprintf('<div>ErrorException <b>%s</b>: closing <b>(null)</b> field</div>', __CLASS__));
+		if ($this->cfg['current_field'] === null) {
+			throw new \Exception('[Cutter]: cannot closing (null) field', 1);
+		}
 
-		// n/next|a/after
-		if (empty($this->cfg['current_stack']) || $this->cfg['current_stack'][0] === 'n' || $this->cfg['current_stack'][0] === 'a') {
-			array_push($this->viewset[$this->current_field], ob_get_clean());
+		/* n/next | a/after */
+		if (empty($this->cfg['current_stack']) || preg_match('/^[na]/', $this->cfg['current_stack'])) {
+			array_push($this->viewset[$this->cfg['current_field']], ob_get_clean());
 
-		// p/previous|b/before
-		} elseif ($this->cfg['current_stack'][0] === 'p' || $this->cfg['current_stack'][0] === 'b') {
+		/* p/previous | b/before */
+		} elseif (preg_match('/^[pb]/', $this->cfg['current_stack'])) {
 			array_unshift($this->viewset[$this->cfg['current_field']], ob_get_clean());
 
-		// oops...
 		} else {
-			throw new Exception(sprintf('<div>ErrorException <b>%s</b>: unknown <b>%s</b> stack position</div>', __CLASS__, $this->cfg['current_stack']));
+			throw new \Exception(sprintf('[Cutter]: unknown "%s" stack position', $this->cfg['current_stack']), 1);
 		}
 
 		$this->cfg['current_field'] = null;
 		$this->cfg['current_stack'] = null;
 	}
+	public function stop()
+	{
+		$this->end();
+	}
 
 	public static function facade()
 	{
-		require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'fn-facade.php');
+		require __DIR__ . '/fn-facade.php';
 	}
 }
 
-function cutter_load_file($_cutter_load_file_path, &$_cutter_load_file_data, $_cutter_load_file_isob) {
+function _cutter_include($_cutter_load_file_path, &$_cutter_load_file_data, $_cutter_load_file_isob) {
 	if ($_cutter_load_file_isob) ob_start();
 	extract($_cutter_load_file_data);
-	include($_cutter_load_file_path);
+	require $_cutter_load_file_path;
 	if ($_cutter_load_file_isob) ob_end_clean();
 }
