56b5d30a6eeae65b46d092444b108bef4cd0bb84
anovsiradj <anov.siradj22@gmail.com>
2018-07-24 22:56:56 +0900

    my OCD strike again!

--- fn-facade.php
+++ fn-facade.php
@@ -1,30 +1,43 @@
 <?php
-
 /**
 * 
-* use anovsiradj/Cutter::init()->facade()
+* by $blade->facade()
 * or anovsiradj/Cutter::facade()
 * to use this functions.
 * 
-* @since 20180602342132
+* @since 20180602342132, 20180724202309
 * 
 */
 
 use anovsiradj\Cutter;
 
-if (function_exists('cutter_field') === false) {
+/* FIELD */
+if (!function_exists('cutter_field')) {
 	function cutter_field($field) {
 		Cutter::init()->field($field);
 	}
 }
 
-if (function_exists('cutter_start') === false) {
-	function cutter_start($name) {
-		Cutter::init()->start($name);
+/* BEGINNING */
+if (!function_exists('cutter_start')) {
+	function cutter_start($name, $stack = null) {
+		Cutter::init()->start($name, $stack);
 	}
 }
 
-if (function_exists('cutter_end') === false) {
+if (!function_exists('cutter_begin')) {
+	function cutter_begin($name, $stack = null) {
+		Cutter::init()->start($name, $stack);
+	}
+}
+
+/* ENDING */
+if (!function_exists('cutter_stop')) {
+	function cutter_stop() {
+		Cutter::init()->end();
+	}
+}
+if (!function_exists('cutter_end')) {
 	function cutter_end() {
 		Cutter::init()->end();
 	}
